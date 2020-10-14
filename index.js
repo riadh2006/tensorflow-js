e0f68982f97979a59cd9d1319dfa522d945361d5
Riadh BEL HADJ SALAH <rsalah@stcs.com.sa>
2020-10-14 13:38:04 +0300

    add / mul operations

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ data2.print();
 data3.assign(tf.tensor2d([[4, 12, 5, 6],[4, 12, 5, 6],[4, 12, 5, 6], [56, 3, 45, 3]]));
 data3.print();
 
+// Create 2 new 1 dimensional tensors 
+const data4 = tf.tensor1d([4, 6, 8, 2]);
+const data5 = tf.tensor1d([7, 9, 18, 22]);
+
+data4.print();
+data5.print();
+
+// Add and multiplies and print 
+data4.add(data5).print();
+data4.mul(data5).print();
